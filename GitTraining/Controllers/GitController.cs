9b3dcdfc01e3420d0627d4369a83f14860bab254
Marc Toye <marc.toye@drs.wa.gov>
2021-01-27 13:57:36 -0800

    Implementated Commands view
    Added Commands view
    Updated Git Controller for new view

--- GitTraining/Controllers/GitController.cs
+++ GitTraining/Controllers/GitController.cs
@@ -13,5 +13,10 @@ namespace GitTraining.Controllers
         {
             return View();
         }
+
+        public ActionResult Commands()
+        {
+            return View();
+        }
     }
 }
